3ab7a4520582bb1af5aebbb1a18b741db2836713
Marco Boretto <marco.bore@gmail.com>
2022-12-19 00:15:50 +0100

    Add min manifest configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,37 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      mode: "development",
+      base: "/",
+      includeAssets: ["/favicon.png"],
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
 })
