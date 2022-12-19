23152e0ca4dba417da6c6a929c0fb55ae0ad8770
Marco Boretto <marco.bore@gmail.com>
2022-12-19 12:28:34 +0100

    Add bigger svg icon

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         background_color: "#ffffff",
         icons: [
           {
-              "src": "vite.svg",
+              "src": "icon.svg",
               "sizes": "48x48 72x72 96x96 128x128 256x256 512x512",
               "type": "image/svg+xml",
               "purpose": "any"
